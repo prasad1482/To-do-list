27d80fd852e92275769678232d48d614a7247482
prasad <djnanasatyaprasad@gmail.com>
2025-04-27 09:53:17 +0530

    basic to list without styling

---
+++ script.js
@@ -0,0 +1,36 @@
+const addBtn= document.getElementById("add-btn");
+const input=document.getElementById("input");
+const list=document.getElementById("list");
+
+addBtn.addEventListener("click",function(){
+    if(input.value.trim()===""){
+        alert("please enter task");
+        return
+    }
+    //create li element
+    const li=document.createElement("li");
+    li.textContent=input.value;
+    //create complte button
+    const comBtn=document.createElement("button");
+    comBtn.textContent="Completed";
+    comBtn.style.marginLeft="10px";
+    comBtn.addEventListener("click",function(){
+        li.style.textDecoration="line-through";
+        comBtn.disabled=true;
+
+    });
+    //create delete button
+    const delBtn=document.createElement("button");
+    delBtn.textContent="Delete";
+    delBtn.style.marginLeft="5px";
+    delBtn.addEventListener("click",function(){
+        li.remove();
+
+    });
+
+    li.appendChild(comBtn);
+    li.appendChild(delBtn);
+    //append li to list
+    list.appendChild(li);
+    input.value="";
+});
